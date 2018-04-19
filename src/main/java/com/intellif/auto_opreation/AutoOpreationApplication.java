317584e928d31039efb941798531c2e224cacc46
maijisheng <mai.jisheng@intellif.com>
2018-04-19 18:17:31 +0800

    2

--- src/main/java/com/intellif/auto_opreation/AutoOpreationApplication.java
+++ src/main/java/com/intellif/auto_opreation/AutoOpreationApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class AutoOpreationApplication {
-	@RequestMapping("/")
+	@RequestMapping("/test")
 	public String index(){
 		return "get start";
 	}
